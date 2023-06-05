b955b8dc69af511255dcc5f1dc2a59efcd784a7c
guzhu@fairygui.com <guzhu@fairygui.com>
2023-06-05 22:58:52 +0800

    fix examples

--- test/src/index.ts
+++ test/src/index.ts
@@ -2,6 +2,9 @@ import '@ungap/custom-elements';
 import * as fgui from "../../build/FairyGUI";
 
 async function start() {
+    //create groot before any UI creation
+    fgui.GRoot.inst;
+
     await fgui.UIPackage.loadPackage("assets/Package1");
 
     let view = fgui.UIPackage.createObject("Package1", "Main");
